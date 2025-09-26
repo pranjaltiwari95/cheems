ec42a301845fca0adc0bd3d2102dafbab94a8e54
pranjaltiwari95 <161000108+pranjaltiwari95@users.noreply.github.com>
2025-09-26 17:09:58 +0530

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -85,7 +85,7 @@ export default function HeroSection() {
 
             <div className="mx-auto max-w-7xl px-6">
               <div className="text-center sm:mx-auto lg:mt-0 lg:mr-auto">
-                <AnimatedGroup variants={transitionVariants}>
+                {/* <AnimatedGroup variants={transitionVariants}>
                   <Link
                     href="#link"
                     className="hover:bg-background dark:hover:border-t-border bg-muted group mx-auto flex w-fit items-center gap-4 rounded-full border p-1 pl-4 shadow-md shadow-zinc-950/5 transition-colors duration-300 dark:border-t-white/5 dark:shadow-zinc-950"
@@ -106,7 +106,7 @@ export default function HeroSection() {
                       </div>
                     </div>
                   </Link>
-                </AnimatedGroup>
+                </AnimatedGroup> */}
 
                 <TextEffect
                   preset="fade-in-blur"
