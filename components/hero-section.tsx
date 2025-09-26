b0951646b8f98ce72ab15e8d948f95a201e1f3d7
pranjaltiwari95 <161000108+pranjaltiwari95@users.noreply.github.com>
2025-09-26 17:01:11 +0530

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -151,22 +151,11 @@ export default function HeroSection() {
                       size="lg"
                       className="rounded-xl px-5 text-base"
                     >
-                      <Link href="#link">
+                      <Link href="/dashboard">
                         <span className="text-nowrap">Adopt a Cheems</span>
                       </Link>
                     </Button>
                   </div>
-                  <Button
-                    key={2}
-                    asChild
-                    size="lg"
-                    variant="ghost"
-                    className="h-10.5 rounded-xl px-5"
-                  >
-                    <Link href="#link">
-                      <span className="text-nowrap">Start as a Shelter</span>
-                    </Link>
-                  </Button>
                 </AnimatedGroup>
               </div>
             </div>
