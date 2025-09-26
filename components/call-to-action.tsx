c6922fe76117650f3284198d4459a8bcbb8bde5d
pranjaltiwari95 <161000108+pranjaltiwari95@users.noreply.github.com>
2025-09-26 17:01:49 +0530

    Update call-to-action.tsx

--- components/call-to-action.tsx
+++ components/call-to-action.tsx
@@ -7,20 +7,14 @@ export default function CallToAction() {
       <div className="mx-auto max-w-5xl rounded-3xl border px-6 py-12 md:py-20 lg:py-32">
         <div className="text-center">
           <h2 className="text-4xl font-semibold text-balance lg:text-5xl">
-            Start Building
+            Adopt a Cheems
           </h2>
-          <p className="mt-4">Libero sapiente aliquam quibusdam aspernatur.</p>
+          <p className="mt-4">Find a Furry Friend Cheems with Cheems</p>
 
           <div className="mt-12 flex flex-wrap justify-center gap-4">
-            <Button asChild size="lg">
-              <Link href="/">
-                <span>Get Started</span>
-              </Link>
-            </Button>
-
-            <Button asChild size="lg" variant="outline">
-              <Link href="/">
-                <span>Book Demo</span>
+            <Button asChild size="lg" className="rounded-xl px-5 text-base">
+              <Link href="/dashboard">
+                <span className="text-nowrap">Adopt a Cheems</span>
               </Link>
             </Button>
           </div>
