ebd50b2c13ea5f019a97d7e16563b0809923a9d4
pranjaltiwari95 <161000108+pranjaltiwari95@users.noreply.github.com>
2025-09-26 17:00:30 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,3 @@
-import { Logo } from "@/components/logo";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -177,7 +176,7 @@ export default function FooterSection() {
         </div>
         <span className="text-muted-foreground block text-center text-sm">
           {" "}
-          © {new Date().getFullYear()} Tailark, All rights reserved
+          © {new Date().getFullYear()} Cheems, All rights reserved
         </span>
       </div>
     </footer>
